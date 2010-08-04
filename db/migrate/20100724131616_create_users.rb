74727d193b0bb805012e5fa9c5281362b07f74b0
Henryk Tunguz <muradin007@gmail.com>
2010-08-04 13:07:33 -0400

    edited create users migration to fix syntax issues, AND OMFG WITH THE NUMBER OF PREVIOUS COMMITS, STUPID HEROKU

--- db/migrate/20100724131616_create_users.rb
+++ db/migrate/20100724131616_create_users.rb
@@ -17,10 +17,10 @@ class CreateUsers < ActiveRecord::Migration
     t.string    :perishable_token,    :null => false                # optional, see Authlogic::Session::Perishability
     t.integer   :login_count,         :null => false, :default => 0 # optional, see Authlogic::Session::MagicColumns
     t.integer   :failed_login_count,  :null => false, :default => 0 # optional, see Authlogic::Session::MagicColumns
-    t.datetime  :last_request_at,                                    # optional, see Authlogic::Session::MagicColumns
-    t.datetime  :current_login_at,                                   # optional, see Authlogic::Session::MagicColumns
-    t.datetime  :last_login_at,                                      # optional, see Authlogic::Session::MagicColumns
-    t.string    :current_login_ip,                                   # optional, see Authlogic::Session::MagicColumns
+    t.datetime  :last_request_at                                    # optional, see Authlogic::Session::MagicColumns
+    t.datetime  :current_login_at                                   # optional, see Authlogic::Session::MagicColumns
+    t.datetime  :last_login_at                                      # optional, see Authlogic::Session::MagicColumns
+    t.string    :current_login_ip                                   # optional, see Authlogic::Session::MagicColumns
     t.string    :last_login_ip                                      # optional, see Authlogic::Session::MagicColumns
     t.timestamps
     end
