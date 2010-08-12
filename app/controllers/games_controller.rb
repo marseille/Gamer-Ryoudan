0a4a8d77a48c057d7759e6de94022f04cc9eea16
Henryk Tunguz <muradin007@gmail.com>
2010-08-11 20:04:48 -0400

    styling and addition of search functionality on home page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,12 +1,15 @@
 class GamesController < ApplicationController
   def find_game    
     @game = Game.find_by_name(params["search_tag"])
-    if @game
-      render :file => "games/index.html", :layout => "application"
+    pp params
+    if @game      
+      render :file => "games/index.html", :layout => "application" if params["load_page"].eql?("true")
+      render :partial => "games/search_results" if !params["load_page"].eql?("true")
     else
       flash[:notice] = "Couldn't find your game, add it to the database nao!"
       @name = params["search_tag"]
-      render :file => "games/new_game.html.erb", :layout => "application"
+      render :file => "games/new_game.html.erb", :layout => "application" if params["load_page"].eql?("true")
+      render :partial => "shared/add_game_and_to_list", :locals => {:only_submit => true} if !params["load_page"].eql?("true")
     end
   end
   
