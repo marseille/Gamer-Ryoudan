7b5f6886b97c50b42e0fea3b726103fa7ee7c5e5
Marseille <Muradin007@gmail.com>
2010-11-01 19:57:25 -0400

    added ruby-github as a dependency

--- config/environment.rb
+++ config/environment.rb
@@ -26,6 +26,7 @@ Rails::Initializer.run do |config|
    config.gem 'authlogic', :version => "2.1.6"
    config.gem 'mysql', :version => "2.8.1"
    config.gem 'rails', :version => "2.2.3"
+   config.gem 'ruby-github'
    #config.gem 'rmagick', :version => "2.13.1" #need some special libraries for this one
    
   # Only load the plugins named here, in the order given. By default, all plugins 
