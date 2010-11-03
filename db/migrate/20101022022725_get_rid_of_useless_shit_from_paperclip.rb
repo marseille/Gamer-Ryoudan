4a8c0fe05ada2c7f8d6793d7f17476ce1d80e0f9
Marseille <Muradin007@gmail.com>
2010-11-03 17:58:34 -0400

    removed line that tried to remove column that doesn't exist

--- db/migrate/20101022022725_get_rid_of_useless_shit_from_paperclip.rb
+++ db/migrate/20101022022725_get_rid_of_useless_shit_from_paperclip.rb
@@ -3,8 +3,7 @@ class GetRidOfUselessShitFromPaperclip < ActiveRecord::Migration
     remove_column :users, :avatar_file_name
     remove_column :users, :avatar_content_type
     remove_column :users, :avatar_file_size
-    remove_column :users, :avatar_updated_at
-    remove_column :users, :avatar_file
+    remove_column :users, :avatar_updated_at    
     remove_column :users, :avatar_medium_file
     remove_column :users, :avatar_thumb_file    
   end
