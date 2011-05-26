9c394df4787324054d07120442e0f6dc3219a8d8
marseille <gamer.ryoudan@gmail.com>
2011-05-26 16:45:07 -0400

    the problem is not the password but accessing ENV

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,13 +22,12 @@ config.action_mailer.raise_delivery_errors = true
 # Enable serving of images, stylesheets, and javascripts from an asset server
 # config.action_controller.asset_host                  = "http://assets.example.com"
 
-#ENV["email_pass"]
 config.action_mailer.smtp_settings = {
     :address              => 'smtp.gmail.com',
     :port                 => 587,
     :domain               => 'gmail.com',
     :user_name            => 'do.not.reply.gamer.ryoudan@gmail.com',
-    :password             => "something12",
+    :password             => ENV["EMAIL_PASS"],
     :authentication       => 'login',
     :enable_starttls_auto => true
   }
